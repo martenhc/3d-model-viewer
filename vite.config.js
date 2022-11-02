9a714828f276f7662f270436342fa782b7de8283
Martín Termini <martin.termini@mediamonks.com>
2022-11-02 13:18:44 +0100

    remove unnecessary package + fix build

--- vite.config.js
+++ vite.config.js
@@ -1,39 +1,21 @@
 import {defineConfig} from 'vite';
+import {resolve as pathResolve} from 'path';
 import del from 'rollup-plugin-delete';
-import replace from '@rollup/plugin-replace';
 import resolve from '@rollup/plugin-node-resolve';
 import summary from 'rollup-plugin-summary';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import {resolve as pathResolve} from 'path';
 
-export default defineConfig(({command, mode}) => {
-  const isPwa = process.env.buildType === 'pwa';
-
-  isPwa &&
-    console.log(
-      '\x1b[32m',
-      `>> You are running the PWA build process.
-       >> Your build will containt a service worker to provide the necessary behavior.`
-    );
-
+export default defineConfig(({command}) => {
   const vitePlugins = [tsconfigPaths()];
 
   const rollupOptions = {
-    plugins: [
-      del({targets: 'build/*'}),
-      replace({
-        // change the value to 'development' if you want to log more errors once built.
-        'process.env.NODE_ENV': JSON.stringify(mode),
-        preventAssignment: true,
-      }),
-      resolve(),
-      summary(),
-    ],
+    plugins: [del({targets: 'build/*'}), resolve(), summary()],
   };
 
   if (command === 'build') {
     return {
       plugins: [...vitePlugins],
+      publicDir: false,
       build: {
         rollupOptions,
         lib: {
@@ -42,7 +24,6 @@ export default defineConfig(({command, mode}) => {
           fileName: '3d-model-viewer',
         },
         outDir: 'build',
-        assetsDir: 'asset',
         minify: 'terser',
         TerserOptions: {
           ecma: 2020,
@@ -50,12 +31,10 @@ export default defineConfig(({command, mode}) => {
           warnings: true,
         },
       },
-      envPrefix: 'VAR_',
     };
   } else if (command === 'serve') {
     return {
       plugins: vitePlugins,
-      envPrefix: 'VAR_',
       build: {
         outDir: 'build',
       },
